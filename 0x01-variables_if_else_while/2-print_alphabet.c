b5d1b9ac7146551b3285a6a5026b18186d05459d
Zienab-Abdelsamea <zienababdelsamea2410@gmail.com>
2023-03-20 13:08:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchat(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
