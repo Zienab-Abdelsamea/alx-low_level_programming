554c658c12b23b7ba1a7983d10fb39e8b324e525
Zienab-Abdelsamea <zienababdelsamea2410@gmail.com>
2023-03-22 16:54:52 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	s = "_putchar";
+
+	for (int i = 0 ; i <= strlen(s) ; i++)
+	{
+		printf(i)
+	}
+	return (0);
+}
+
